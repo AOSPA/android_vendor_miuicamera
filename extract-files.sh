b45569b4202db809ca20a9e790087901e99afdc6
Jyotiraditya <jyotiraditya@aospa.co>
2020-07-05 04:50:56 +0200

    Fix our copyright
    Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>
    Change-Id: If146bac16740cf827d901c1723cf6dcd47458334

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2017-2020 The LineageOS Project
-# Copyright (C) 2020 The AOSPA Project
+# Copyright (C) 2020 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
